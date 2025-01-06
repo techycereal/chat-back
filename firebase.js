f75623eca485678cacb66ab2bded9e8e3f4115c6
techycereal <alexjmilliken@outlook.com>
2025-01-06 10:15:18 -0500

    even more secure

--- firebase.js
+++ firebase.js
@@ -4,10 +4,12 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY
 const PRIVATE_KEY_ID = process.env.PRIVATE_KEY_ID
 const CLIENT_ID = process.env.CLIENT_ID
 const CLIENT_EMAIL = process.env.CLIENT_EMAIL
+const CLIENT_CERT = process.env.CLIENT_CERT
+const PROJECT_ID = process.env.PROJECT_ID
 admin.initializeApp({
   credential: admin.credential.cert({
     "type": "service_account",
-    "project_id": "chatapp-e26b8",
+    "project_id": PROJECT_ID,
     "private_key_id": PRIVATE_KEY_ID,
     "private_key": PRIVATE_KEY.replace(/\\n/g, '\n'),
     "client_email": CLIENT_EMAIL,
@@ -15,7 +17,7 @@ admin.initializeApp({
     "auth_uri": "https://accounts.google.com/o/oauth2/auth",
     "token_uri": "https://oauth2.googleapis.com/token",
     "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-56umm%40chatapp-e26b8.iam.gserviceaccount.com",
+    "client_x509_cert_url": CLIENT_CERT,
     "universe_domain": "googleapis.com"
   }
   )
