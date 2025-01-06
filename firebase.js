4f88aeb10e2614ed11c84867415e53c4ba029f23
techycereal <alexjmilliken@outlook.com>
2025-01-06 10:14:07 -0500

    more secure

--- firebase.js
+++ firebase.js
@@ -3,14 +3,14 @@ var admin = require("firebase-admin");
 const PRIVATE_KEY = process.env.PRIVATE_KEY
 const PRIVATE_KEY_ID = process.env.PRIVATE_KEY_ID
 const CLIENT_ID = process.env.CLIENT_ID
-
+const CLIENT_EMAIL = process.env.CLIENT_EMAIL
 admin.initializeApp({
   credential: admin.credential.cert({
     "type": "service_account",
     "project_id": "chatapp-e26b8",
     "private_key_id": PRIVATE_KEY_ID,
     "private_key": PRIVATE_KEY.replace(/\\n/g, '\n'),
-    "client_email": "firebase-adminsdk-56umm@chatapp-e26b8.iam.gserviceaccount.com",
+    "client_email": CLIENT_EMAIL,
     "client_id": CLIENT_ID,
     "auth_uri": "https://accounts.google.com/o/oauth2/auth",
     "token_uri": "https://oauth2.googleapis.com/token",
